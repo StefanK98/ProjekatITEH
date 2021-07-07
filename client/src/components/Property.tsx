30091813c08be6f090f12cf433cc10eb6f97f76f
StefanK98 <kujovicstefan98@gmail.com>
2021-07-07 11:30:07 +0200

    Finalna verzija aplikacije

---
+++ client/src/components/Property.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+
+interface Props {
+    name: string,
+    value: string | number
+}
+
+export default function Property(props: Props) {
+    return (
+        <Grid.Row columns='16'>
+            <Grid.Column width='6'>{props.name}</Grid.Column>
+            <Grid.Column width='10'>{props.value}</Grid.Column>
+        </Grid.Row>
+    )
+}
